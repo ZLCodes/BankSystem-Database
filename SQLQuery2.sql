75581de2fd627f8189e6054b564e82b20d88b7c0
朱林 <2237404762@qq.com>
2018-01-13 11:27:12 +0800

    银行卡信息录入初始化

---
+++ SQLQuery2.sql
@@ -0,0 +1,58 @@
+insert into Account
+values(1,'6212263602015645300','1')
+insert into Account
+values(2,'6212263602015645301','1')
+insert into Account
+values(3,'6212263602015645302','1')
+insert into Account
+values(4,'6212263602015645303','1')
+insert into Account
+values(5,'6212263602015645304','1')
+insert into Account
+values(6,'6212263602015645305','1')
+insert into Account
+values(7,'6212263602015645306','1')
+insert into Account
+values(8,'6212263602015645307','1')
+insert into Account
+values(9,'6212263602015645308','1')
+insert into Account
+values(10,'6212263602015645309','1')
+insert into Account
+values(11,'6212263602015645310','1')
+insert into Account
+values(12,'6212263602015645311','1')
+insert into Account
+values(13,'6212263602015645312','1')
+insert into Account
+values(14,'6212263602015645313','1')
+insert into Account
+values(15,'6212263602015645314','1')
+insert into Account
+values(16,'6212263602015645315','1')
+insert into Account
+values(17,'6212263602015645316','1')
+insert into Account
+values(18,'6212263602015645317','1')
+insert into Account
+values(19,'6212263602015645318','1')
+insert into Account
+values(20,'6212263602015645319','1')
+insert into Account
+values(21,'6212263602015645320','1')
+insert into Account
+values(22,'6212263602015645321','1')
+insert into Account
+values(23,'6212263602015645322','1')
+insert into Account
+values(24,'6212263602015645323','1')
+insert into Account
+values(25,'6212263602015645324','1')
+insert into Account
+values(26,'6212263602015645325','1')
+
+delete from CardInfo
+delete from zhuanzhang
+delete from Store
+delete from Account
+delete from UserInfo
